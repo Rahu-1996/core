0ad40a95a9221f01807329ca135abbffafab1965
Rahu-1996 <choudhary.rahul4796@gmail.com>
2023-07-20 12:14:48 +0530

    done

---
+++ ArrayClass.java
@@ -0,0 +1,24 @@
+
+public class ArrayClass {
+	
+	public static void main (String[]args) {
+		int[]arr= {10,1,9,4,6,4,3,1};
+		
+		int a=arr[0];
+		
+		for(int i=0; i<arr.length; i++)
+		{	
+			if(arr[0]>arr[i])
+		{
+				a=arr[i];
+				
+		}
+		
+	}
+		System.out.println("min value="+a);
+}
+}
+		
+
+
+
---
+++ ArrayTable.java
@@ -0,0 +1,23 @@
+
+public class ArrayTable {
+	
+	public static void main(String[]args) 
+	{
+      int i,j,k,l;
+		for( i=1;i<=10;i++) {
+			
+			for(j=1;j<=20;j++) {
+				
+				System.out.print(i*j+"\t");
+			
+			}
+			
+			System.out.println();
+			System.out.println();
+		}
+		
+	}
+
+}
+
+
